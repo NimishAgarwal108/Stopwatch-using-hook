121b9e79bd0dd20d8f3f60c80cf59cb87ecb99f6
Nimish Agarwal <agarwalnimish428@gmail.com>
2025-09-05 00:58:16 +0530

    first commit

---
+++ stopwatch/src/App.jsx
@@ -0,0 +1,46 @@
+import { useRef, useState } from 'react'
+import styles from './components/Main.module.css';
+import '../index.css'
+
+function App() {
+  const[time,setTime]=useState(0);
+
+  let refTimer=useRef(null);
+
+
+  function startTimer(){
+    refTimer.current=setInterval(()=>{
+      setTime(time=>time+1)
+    },1000);
+  }
+ 
+  function stopTimer(){
+    clearInterval(refTimer.current);
+    refTimer.current=null;
+  }
+  
+ function resetTimer(){
+    stopTimer();
+    setTime(0);
+  }
+  
+
+  return (
+
+    <div className={styles['main-container']}>
+    <h1>Stopwatch:{time} Seconds</h1>
+
+    <button style={{fontSize:'50px',borderRadius:'10px',marginBottom:'30px', backgroundColor:'green'}} onClick={startTimer}>start</button>
+    <br />
+     <button style={{fontSize:'50px',borderRadius:'10px',marginBottom:'30px', backgroundColor:'red'}} onClick={stopTimer}>stop</button>
+     <br />
+      <button style={{fontSize:'50px',borderRadius:'10px',backgroundColor:'blue'}} onClick={resetTimer}>Reset</button>
+
+    </div>
+
+      
+  
+  )
+}
+
+export default App;
